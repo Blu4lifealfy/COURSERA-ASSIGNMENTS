86ffccf503f4e5c1e89d2b98c1e32ed8f206f78e
Alfred Banjoko <alfredshs@gmail.com>
2021-12-03 14:06:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-(function (window) {
+(function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  var helloSpeaker = {};
-  window.helloSpeaker = helloSpeaker;
-
-for (var i = 0; i < names.length; i++) {};
-	  var firstLetter = names[i].charAt(0).toLowerCase();
-
-	if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+  
+    if (firstLetter === 'j') {
+      byeSpeaker(names[i]);
+    }
+    else {
+      helloSpeaker(names[i]);
+    }
   }
-}
+  
+  })();
+  
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
 
-})();
+})(window);
 
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
 
+	window.helloSpeaker = helloSpeaker;
 
-  function speak(name) {
-  console.log(speakWord + " " + name);
-}
-  var speakWord = "Hello";
-  window.speakWord = speakWord;
-
-	           window.names = names;})
-var byeSpeaker = {};
-window.byeSpeaker = byeSpeaker;
-
-
-(window);
-
-
+})(window);
 
